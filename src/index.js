40cd09b87dc542606429f5d56309873657b67b99
Ivan <i85chiosa@gmail.com>
2022-05-23 15:30:18 +0200

    Currency

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
+const axios = require("axios");
+
+
+const host = 'api.frankfurter.app';
+axios.get(`https://${host}/latest?amount=10&from=GBP&to=USD`)
+  .then(resp => console.log(resp.data))
+  .catch(function (error) {console.error(error)});
+
+
+
 

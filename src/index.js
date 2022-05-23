479880c5607da19bc773fc6dd977d60309b4cd3d
Oxana Danilova <oxanadanilova11@gmail.com>
2022-05-23 16:10:57 +0200

    feat: create method Hotels

--- src/index.js
+++ src/index.js
@@ -1,5 +1,59 @@
-/* #!/usr/bin/env node
- */
-import { sum } from "./currencyConverter.js";
+#!/usr/bin/env node
+const axios = require("axios");
 
-console.log(sum(5, 4));
+class Vacation {
+  constructor(city, amount) {
+    this.city = city;
+    this.amount = amount;
+  }
+  weather() {
+    let API_Key = "b0289e1b34874bb29ea72328221905";
+    let baseUrl = "http://api.weatherapi.com/v1";
+
+    let endPoint = `${baseUrl}/current.json?key=${API_Key}&q=${this.city}`;
+    axios
+      .get(endPoint)
+      .then((res) =>
+        console.log(
+          `${res.data.location.name} - ${res.data.current.condition.text} ${res.data.current.temp_c}°C.`
+        )
+      )
+      .catch((err) => console.log(err));
+  }
+
+  currency() {
+    const host = "api.frankfurter.app";
+    axios
+      .get(`https://${host}/latest?${this.amount}&from=GBP&to=USD`)
+      .then((resp) => console.log(resp.data))
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+  hotels() {
+    console.log(this.city);
+    const options = {
+      method: "GET",
+      url: "https://hotels4.p.rapidapi.com/locations/v2/search",
+      params: { query: this.city /* , locale: "en_US", currency: "USD" */ },
+      headers: {
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+        "X-RapidAPI-Key": "a745873c88mshf27140e07b78acdp10694ajsna86646c2062d",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        console.log(response.data.suggestions[0].entities);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+}
+const newDestination = new Vacation(process.argv.slice(2)[0]);
+//newDestination.weather();
+//newDestination.currency();
+newDestination.hotels();

f772ad4e97e6c58fed97721726742fbe5683b986
Mery Girman <92925333+aboutthatmery@users.noreply.github.com>
2022-05-24 15:16:24 +0200

    schoolHolidayGermany
    Co-authored-by: Sabina Gudzenova <96171137+saby-gaby@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -35,8 +35,34 @@ axios
 
   .catch(function (error) {console.error(error)});
   }
+  schulferien(){
+    const options = {
+  method: 'GET',
+  url: 'https://schulferien-und-feiertage.p.rapidapi.com/school-holidays/next',
+  params: {state: 'BE', limit: '3', lang: 'de'},
+  headers: {
+    'X-RapidAPI-Host': 'schulferien-und-feiertage.p.rapidapi.com',
+    'X-RapidAPI-Key': '255165c0e8mshf08490884766bf7p166332jsnd98856d09c9d'
+  }
+};
+
+axios.request(options)
+.then(function (response) {
+  const arr = response.data.data;
+  arr.forEach((el)=> {
+    console.log(el.name);
+    console.log(el.start);
+    console.log(el.end);
+    console.log(el.state)
+    console.log(el.duration);
+  })	
+}).catch(function (error) {
+	console.error(error);
+});
+  }
 }
 const newDestination = new Vacation(process.argv.slice(2)[0],process.argv.slice(2)[1]);
 
 newDestination.weather();
 newDestination.currency();
+

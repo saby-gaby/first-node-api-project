47daf9776f09d2a255a5c619d85c097f4cec92db
Ivan <i85chiosa@gmail.com>
2022-05-24 15:44:16 +0200

    Correctur

--- src/index.js
+++ src/index.js
@@ -23,47 +23,48 @@ class Vacation {
 
 
 
-currency(){
-const host = 'api.frankfurter.app';
-axios
-  .get(`https://${host}/latest?to=USD,GBP,RON,CZK&amount=${this.amount}`)
-  .then((resp)=>
-    {
-      console.log(
-        `On the date of ${resp.data.date}, amount of: ${resp.data.amount}-${resp.data.base} is: USD ${resp.data.rates.USD}, GBP ${resp.data.rates.GBP}, RON ${resp.data.rates.RON} Thank you and have a good day from your team Ivan, Oxana, Sabina and Mercedes!`
-      )
-    })
+  currency() {
+    const host = 'api.frankfurter.app';
+    axios
+      .get(`https://${host}/latest?to=USD,GBP,RON,CZK&amount=${this.amount}`)
+      .then((resp) => {
+        console.log(
+          `On the date of ${resp.data.date}, amount of: ${resp.data.amount}-${resp.data.base} is: USD ${resp.data.rates.USD}, GBP ${resp.data.rates.GBP}, RON ${resp.data.rates.RON} Thank you and have a good day from your team Ivan, Oxana, Sabina and Mercedes!`
+        )
+      })
 
-  .catch(function (error) {console.error(error)});
+      .catch(function (error) { console.error(error) });
   }
-  schulferien(){
+  schulferien() {
     const options = {
-  method: 'GET',
-  url: 'https://schulferien-und-feiertage.p.rapidapi.com/school-holidays/next',
-  params: {state: 'BE', limit: '3', lang: 'de'},
-  headers: {
-    'X-RapidAPI-Host': 'schulferien-und-feiertage.p.rapidapi.com',
-    'X-RapidAPI-Key': '255165c0e8mshf08490884766bf7p166332jsnd98856d09c9d'
-  }
-};
+      method: 'GET',
+      url: 'https://schulferien-und-feiertage.p.rapidapi.com/school-holidays/next',
+      params: { state: 'BE', limit: '3', lang: 'de' },
+      headers: {
+        'X-RapidAPI-Host': 'schulferien-und-feiertage.p.rapidapi.com',
+        'X-RapidAPI-Key': '255165c0e8mshf08490884766bf7p166332jsnd98856d09c9d'
+      }
+    };
 
-axios.request(options)
-.then(function (response) {
-  const arr = response.data.data;
-  arr.forEach((el)=> {
-    console.log(el.name);
-    console.log(el.start);
-    console.log(el.end);
-    console.log(el.state)
-    console.log(el.duration);
-  })	
-}).catch(function (error) {
-	console.error(error);
-});
+    axios.request(options)
+      .then(function (response) {
+        const arr = response.data.data;
+        arr.forEach((el) => {
+          console.log(el.name);
+          console.log(el.start);
+          console.log(el.end);
+          console.log(el.state)
+          console.log(el.duration);
+        })
+      }).catch(function (error) {
+        console.error(error);
+      });
   }
 }
-const newDestination = new Vacation(process.argv.slice(2)[0],process.argv.slice(2)[1]);
+const newDestination = new Vacation(...process.argv.slice(2));
 
 newDestination.weather();
 newDestination.currency();
+newDestination.schulferien();
+
 
